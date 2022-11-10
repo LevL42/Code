0e2ce5fdc348d982e0f3a36d538846f7444651d3
Lev Likhachev <lev.i.lena@yandex.ru>
2022-11-10 14:09:36 +0700

    Example003 Sum Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,10 @@
-﻿int numberA = 33;
+﻿/*int numberA = 33;
 int numberB = 5;
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine(result);*/
+
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
