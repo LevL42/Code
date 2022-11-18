c0cb38fc5f34c9f5c56814e838556a33f1b42253
Lev Likhachev <lev.i.lena@yandex.ru>
2022-11-18 12:17:57 +0700

    Example010 Method 2

---
+++ Example010_method2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 10, 22, 33, 4, 9, 55, 60, 78, 83, 9 };
+
+int n = array.Length;
+int find = 9;
+
+int index = 0;
+while (index < find)
+{
+
+    if (array[index] == find)
+    {
+
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
